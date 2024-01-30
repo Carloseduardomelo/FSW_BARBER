89925e7a163f9c6a0fd49975ed8a9c7eccd1b3e5
Carloseduardomelo <edu464377@gmail.com>
2024-01-30 15:18:24 -0300

    feat: add welcom mensagen

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,10 +1,21 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../_components/Header";
+import { ptBR } from "date-fns/locale";
+import { Input } from "../_components/ui/input";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="py-6 px-5">
+        <h2 className="font-bold text-xl">Ola! Carlos</h2>
+        <p className="text-sm">
+          {format(new Date(), "EEEE ',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
+      <div className="px-5">
+        <Input type="email" placeholder="Busque seu Coll" />
+      </div>
     </div>
   );
 }
