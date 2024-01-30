4db559074ad5d661c354e0bf0c1c83eebbf09788
Carloseduardomelo <edu464377@gmail.com>
2024-01-30 19:24:16 -0300

    feat: crate conponent Barbershopitem

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -4,7 +4,13 @@ import { ptBR } from "date-fns/locale";
 import  Search  from "./_conponents/Search"
 import Bookin_item from "../_components/Bookin-item";
 
-export default function Home() {
+import { db } from "../_lib/prisma";
+import { BarbershopItem } from "./_conponents/Barbershop_item";
+
+export default async function Home() {
+
+  const Barbershop = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
@@ -18,9 +24,18 @@ export default function Home() {
         <Search />
       </div>
       <div className="px-5 pt-8 flex flex-col gap-3">
-        <h1 className="text-sm uppercase">corde de cabelo</h1>
+        <h1 className="text-sm uppercase font-bold text-gray-400">corde de cabelo</h1>
         <Bookin_item />
       </div>
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {Barbershop.map((barbershop: any) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  ); 
 }
