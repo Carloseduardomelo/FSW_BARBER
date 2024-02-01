ec9b1b47ec914f4d7ff585cc3fd9310a89146633
Carloseduardomelo <edu464377@gmail.com>
2024-02-01 14:14:52 -0300

    feat: create menu

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,28 +1,106 @@
-"use client"
-
+"use client";
 
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
-import { signIn } from "next-auth/react";
+import { ArrowRightToLine, Calendar, CalendarDays, CircleUserIcon, Home, MenuIcon } from "lucide-react";
+import { signIn, useSession , signOut } from "next-auth/react";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetClose,
+  SheetDescription,
+  SheetTitle,
+  SheetFooter,
+} from "./ui/sheet";
+import { Avatar , AvatarImage , AvatarFallback } from "./ui/avatar";
 
 const Header = () => {
+  const handleLoginClick = async () => {
+    await signIn();
+  };
 
-  const handleLoginClick = async () =>{
-    await signIn()
+  const HandleSairClick = async () => {
+    await signOut()
   }
+
+  const { data } = useSession();
+
+  const name = `${data?.user?.name?.split(" ")[0]} ${data?.user?.name?.split(" ")[1]}`
 
   return (
     <Card>
       <CardContent className="py-6 px-5 flex justify-between items-center text-center">
-        <Image src={"/logo.png"} alt="Logo do projeto barber." height={22} width={128}/>
-        {/* <Button variant={"outline"} size={"icon"} className="p-3">
-          <MenuIcon />
-        </Button> */}
-        <Button onClick={handleLoginClick}>
-          logar
-        </Button>
+        <Image
+          src={"/logo.png"}
+          alt="Logo do projeto barber."
+          height={22}
+          width={128}
+        />
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"outline"} size={"icon"} className="p-3">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="p-0 flex flex-col m-0">
+            <SheetHeader className="">
+              <SheetTitle className="text-left w-full border-b border-solid border-secondary p-5 flex items-center">
+                Menu
+              </SheetTitle>
+            </SheetHeader>
+            {!data?.user ? (
+              <div className="px-5  flex flex-col gap-4">
+                <div className="flex gap-3 items-center text-[14px] font-bold">
+                  <CircleUserIcon className="text-gray-500" size={32} />
+                  <p>Olà!, Faça seu login</p>
+                </div>
+                <div>
+                  <Button
+                    variant={"secondary"}
+                    className="flex gap-3 w-full items-center justify-start"
+                    onClick={() => handleLoginClick()}
+                  >
+                    <ArrowRightToLine size={16} />
+                    <p>Fazer Login</p>
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              <div className="px-5 flex justify-between">
+                <div className="flex items-center gap-3">
+                  <Avatar>
+                    <AvatarImage
+                      src={data.user.image ?? ""}
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar> 
+                  <h2>{name}</h2>
+                </div>
+                <Button variant={"secondary"} onClick={() => HandleSairClick()}>
+                  <ArrowRightToLine size={16}/>
+                </Button>
+              </div>
+            )}
+
+            <SheetFooter className="p-5">
+              <SheetClose className="flex flex-col gap-3">
+                <Button variant={"secondary"} className="flex gap-3 items-center justify-start bg-transparent border border-solid border-secondary w-full ">
+                  <Home size={16}/>
+                  <p>Inicio</p>
+                </Button>
+                <Button variant={"secondary"} className="flex gap-3 items-center justify-start bg-transparent border border-solid border-secondary w-full ">
+                  <CalendarDays size={16}/>
+                  <p>Agendamentos</p>
+                </Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   );
